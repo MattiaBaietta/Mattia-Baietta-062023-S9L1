9673499f77ee7f5fd6377082ed400a4005cd65f8
MattiaBaietta <mattia.baietta89@gmail.com>
2023-12-18 16:01:26 +0100

    s9l1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassComponent from './components/ImageComponent';
 
+import Button from './components/ButtonComponents';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Button testo="ciao"/>
+      <ClassComponent src="https://www.backmarket.it/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D1920/https://d2e6ccujb3mkqf.cloudfront.net/943afa80-1922-43de-a7d9-8c1b4294a727-1_57539676-9a3c-465d-85d8-5a8f811d04dc.jpg" alt="immagine iphone"/>
     </div>
   );
 }
---
+++ src/components/ButtonComponents.jsx
@@ -0,0 +1,9 @@
+
+
+const Button = (props) =>{
+    return(
+        <button>{props.testo}</button>
+    )
+}
+
+export default Button;
